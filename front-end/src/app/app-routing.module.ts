471cd41e2cf612402139f5344c8f48c351db9e6c
tetiana.molchanova-at-022587608743 <tetiana.molchanova@edetek.com>
2021-05-13 08:29:04 +0300

    markup main page

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {RegComponent} from "./reg/reg.component";
-import {AuthComponent} from "./auth/auth.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
+import { HomeComponent } from "./home/home.component";
+import { RegComponent } from "./reg/reg.component";
+import { AuthComponent } from "./auth/auth.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AuthGuard } from "./auth.guard";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'reg', component: RegComponent },
   { path: 'auth', component: AuthComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
