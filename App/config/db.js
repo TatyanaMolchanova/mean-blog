8ba4f3e11d6fa3a733616d0e98d4b5d6a4e80935
tetiana.molchanova-at-022587608743 <tetiana.molchanova@edetek.com>
2021-05-14 14:26:05 +0300

    ready blog whole

--- App/config/db.js
+++ App/config/db.js
@@ -1,8 +1,9 @@
 // File for connecting to database and secret key
 module.exports = {
     // connecting to db - db: 'mongodb://locallhost:27017/blog',
-    db: 'mongodb://localhost:27017/blog',
+    // db: 'mongodb://localhost:27017/blog',
     // db: 'mongodb+srv://tanya:ejpKB8*8Jh$KWHb@cluster0.yeavq.mongodb.net/meanBlog',
+    db: 'mongodb+srv://tanya:ejpKB8*8Jh$KWHb@cluster0.kzolz.mongodb.net/Mean-blog?retryWrites=true&w=majority',
     // secret key
     secret: 'some_secret_key'
 }
